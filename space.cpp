5d21587b20ae5c6cff3f92a372d28b1607adfa4d
Park JuneKyu <pjk0007@naver.com>
2018-11-18 00:52:16 +0900

    add space.h and space.cpp

---
+++ space.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "space.h"
+using namespace std;
+
+space::space()
+{
+	start_Time = 0;
+	end_Time = 24;
+}
+
+
---
+++ space.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#ifndef _SPACE_H
+#define _SPACE_H
+using namespace std;
+
+class space
+{
+	protected:
+		int start_Time;
+		int end_Time;
+		int borrowed_Time;
+		int return_Time;
+		
+	public:
+		space();
+};
+
+class study_room : public space
+{
+	protected:
+		int exist;	// 0 is already borrowed, 1 is exist
+
+	public :
+		study_room() : space(){
+			start_Time = 9;
+			end_Time = 18;
+			exist = 1;
+		}
+};
+
+class seat : public space
+{
+	protected:
+		int remain;	// maximum 50 seats
+
+	public :
+		seat(int st, int et) : space(){
+			start_Time = st;
+			end_Time = et;
+			remain = 0;
+		}
+};
+#endif
